85557bf5d7b5f3add50b6085c24511636539a420
Manamani Soundous <se.m.soundous@gmail.com>
2024-05-19 18:31:21 +0100

    added login and signup backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('signup-login');
 });
+
+Route::post('/create-user', [userController::class, 'create_user']);
+Route::get('/create-user', function () {
+
+    return redirect()->route('signup-login');
+});
+
+Route::post('/login', [UserController::class, 'login']);
