787bc3b495a5377640923c82aa1d2136fac7fb4f
Manamani Soundous <se.m.soundous@gmail.com>
2024-05-21 21:28:22 +0100

    added admin request table and model

---
+++ app/Models/AdminRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminRequest extends Model
+{
+    use HasFactory;
+    protected $table = 'admin_requests';
+    protected $fillable = [
+        'user_id',
+    ];
+
+    protected $primaryKey = 'id';
+
+    public $timestamps = true;
+
+    /**
+     * Relationship with User model
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Borrow::class);
     }
+
+    public function admin_request()
+    {
+        return $this->hasOne(AdminRequest::class);
+    }
 }
